0b182d0d84dabfbe6f6c4056b96e573e23e4b561
paolaengelberger <paoe2006@gmail.com>
2023-06-06 23:58:46 -0400

    personalización de la tabla

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ st.markdown(" :violet[This app will generate graphs from frecuency tables ;)]")
 
 df = pd.DataFrame(
     [
-        {"command": "Dato 1", "Frecuencia":0, },
-        {"command": "Dato 2", "Frecuencia":0 , },
-        {"command": "Dato 3", "Frecuencia":0, },
+        {"Dato":, "Frecuencia":0, },
+        {"Dato":, "Frecuencia":0, },
+        {"Dato":, "Frecuencia":0, },
     ]
 ) 
 

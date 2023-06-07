1ca6594c50093020b33cb16c9dee9740ea77374c
paolaengelberger <paoe2006@gmail.com>
2023-06-07 00:32:45 -0400

    cambio de funciones text and number imput

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import streamlit as st
 import pandas as pd
 
-
 st.title("Welcome to my first app")
-
 st.markdown(":violet[This app will generate graphs from frequency tables ;)]")
+
 df = pd.DataFrame(columns=["Dato", "Frecuencia"])
 
 custom_num_rows = int(input("Enter the number of rows: "))
 
-for _ in range(custom_num_rows):
-    dato = st.text_input("Enter Dato")
-    frecuencia = st.number_input("Enter Frecuencia", min_value=0, value=0, step=1)
-    row = {"Dato": dato, "Frecuencia": frecuencia}
-    df = df.append(row, ignore_index=True)
+user_input = st.text_area("Enter Dato and Frecuencia (separated by a comma)", height=custom_num_rows)
+
+rows = user_input.split("\n")
+
+for row in rows:
+    values = row.split(",")
+    if len(values) == 2:
+        dato = values[0].strip()
+        frecuencia = int(values[1].strip())
+        df = df.append({"Dato": dato, "Frecuencia": frecuencia}, ignore_index=True)
 
 st.table(df)
 

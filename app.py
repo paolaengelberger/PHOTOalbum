14a63ac9531b9c6ac5ff3cc81786f004024e9234
paolaengelberger <paoe2006@gmail.com>
2023-06-07 00:02:13 -0400

    dato 1

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ st.markdown(" :violet[This app will generate graphs from frecuency tables ;)]")
 
 df = pd.DataFrame(
     [
-        {"Dato":, "Frecuencia":0, },
-        {"Dato":, "Frecuencia":0, },
-        {"Dato":, "Frecuencia":0, },
+        {"Dato": "Dato 1", "Frecuencia":0, },
+        {"Dato": "Frecuencia":0, },
+        {"Dato": "Frecuencia":0, },
     ]
 ) 
 
 custom_num_rows = st.number_input("Enter the number of rows", min_value=1, value=len(df))
 edited_df= st.data_editor(df, num_rows="dynamic") 
-favorite_command = edited_df.loc[edited_df["Frecuencia"].idxmax()]["command"]
+favorite_command = edited_df.loc[edited_df["Frecuencia"].idxmax()]["Dato"]
 
 

3abd5ced72812cfb1d8d140c7569a8c055636e2c
paolaengelberger <paoe2006@gmail.com>
2023-06-06 23:47:19 -0400

    Data frame

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 import streamlit as st 
+import pandas as pd
+
 st.title("Welcome to my first app")
 
 st.markdown(" :violet[This app will generate graphs from frecuency tables ;)]")  
+
+
+df = pd.DataFrame(
+    [
+        {"command": "Dato 1", "Frecuencia":0, },
+        {"command": "Dato 2", "Frecuencia":0 , },
+        {"command": "Dato 3", "Frecuencia":0, },
+    ]
+) edited_df = st.data_editor(df, num_rows="dynamic")
+
+

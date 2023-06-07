967d2f7e1f39804cf3f0434e19239e48b42eda63
paolaengelberger <paoe2006@gmail.com>
2023-06-07 00:38:29 -0400

    st. dataframe funcion

--- app.py
+++ app.py
@@ -6,19 +6,12 @@ st.markdown(":violet[This app will generate graphs from frequency tables ;)]")
 
 df = pd.DataFrame(columns=["Dato", "Frecuencia"])
 
-custom_num_rows = int(input("Enter the number of rows: "))
+custom_num_rows = st.number_input("Enter the number of rows", min_value=1, value=0, step=1, key="num_rows")
 
-user_input = st.text_area("Enter Dato and Frecuencia (separated by a comma)", height=custom_num_rows)
+for _ in range(custom_num_rows):
+    row = {"Dato": None, "Frecuencia": None}
+    df = df.append(row, ignore_index=True)
 
-rows = user_input.split("\n")
+editable_df = st.dataframe(df, editable=True)
 
-for row in rows:
-    values = row.split(",")
-    if len(values) == 2:
-        dato = values[0].strip()
-        frecuencia = int(values[1].strip())
-        df = df.append({"Dato": dato, "Frecuencia": frecuencia}, ignore_index=True)
-
-st.table(df)
-
-favorite_command = df.loc[df["Frecuencia"].idxmax(), "Dato"]
+favorite_command = editable_df.loc[editable_df["Frecuencia"].idxmax(), "Dato"]

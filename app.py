6f73b15965f51cd451b028f6cd9b08a188c839fd
paolaengelberger <paoe2006@gmail.com>
2023-06-06 23:29:38 -0400

    generate graphs- marckdown

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st 
 st.title("Welcome to my first app")
 
-st.markdown(" :violet[This app will provide cute figures made up of charts]")  
+st.markdown(" :violet[This app will generate graphs from frecuency tables ;)]")  
